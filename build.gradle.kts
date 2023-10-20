7e885bf45b46a50b71e4f999d0487e75d68be61d
JunHyun Lee <dlwnsgus777@naver.com>
2023-10-20 23:07:35 +0900

    feat: 설정 시도 중..
    feat: 설정 시도 중..

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,99 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jooq.meta.jaxb.ForcedType
+import org.jooq.meta.jaxb.Logging
+import org.jooq.meta.jaxb.Property
 
 plugins {
-	id("org.springframework.boot") version "2.7.16"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+    id("org.springframework.boot") version "2.7.16"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("nu.studer.jooq") version "8.2"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
 }
 
 group = "com.jooq"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    runtimeOnly("mysql:mysql-connector-java:8.0.25")
+    jooqGenerator("mysql:mysql-connector-java:8.0.25")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
+jooq {
+	version.set("3.18.4")  // default (can be omitted)
+	edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)  // default (can be omitted)
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "1.8"
+	configurations {
+		create("main") {  // name of the jOOQ configuration
+			generateSchemaSourceOnCompilation.set(true)  // default (can be omitted)
+
+			jooqConfiguration.apply {
+				logging = Logging.WARN
+				jdbc.apply {
+					driver = "com.mysql.cj.jdbc.Driver"
+					url = "jdbc:postgresql://localhost:3306/practice"
+					user = "root"
+					password = "dlwnsgus123!@#"
+					properties.add(Property().apply {
+						key = "ssl"
+						value = "true"
+					})
+				}
+				generator.apply {
+					name = "org.jooq.codegen.DefaultGenerator"
+					database.apply {
+						name = "org.jooq.meta.mysql.MySQLDatabase"
+						inputSchema = "public"
+						forcedTypes.addAll(listOf(
+							ForcedType().apply {
+								name = "varchar"
+								includeExpression = ".*"
+								includeTypes = "JSONB?"
+							},
+							ForcedType().apply {
+								name = "varchar"
+								includeExpression = ".*"
+								includeTypes = "INET"
+							}
+						))
+					}
+					generate.apply {
+						isDeprecated = false
+						isRecords = true
+						isImmutablePojos = true
+						isFluentSetters = true
+					}
+					target.apply {
+						packageName = "com.jooq.example"
+						directory = "build/generated-src/jooq/main"  // default (can be omitted)
+					}
+					strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+				}
+			}
+		}
 	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "1.8"
+    }
 }
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
